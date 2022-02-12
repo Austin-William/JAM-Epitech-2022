cded357fe366c6b2093fa142da96793a9e2f517e
Austin-William Lo <austin-william.lo@epitech.eu>
2022-02-12 12:00:43 +0100

    [ADD] first page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Error from './pages/Error';
+
+const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <Routes>
+            <Route exact path="/" element={<App />} />
+            <Route
+              path="*"
+              element={<Error />}
+              options={{ status: 404 }}
+            />
+        </Routes>
+    </BrowserRouter>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/Error.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../styles/Error.css";
+
+function ErrorPage() {
+    return (
+        <div className="Error">
+            <h1>Error</h1>
+            <p>
+                This page does not exist.
+            </p>
+        </div>
+    );
+}
+
+export default ErrorPage;
