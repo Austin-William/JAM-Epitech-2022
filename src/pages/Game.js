44770f9b62cc3c4f87bad1d776e69024e4f55657
Austin-William Lo <austin-william.lo@epitech.eu>
2022-02-12 15:59:27 +0100

    [ADD] animation

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -4,22 +4,25 @@ import '../styles/Game.scss';
 function Game() {
 
     const [dialog, setDialog] = React.useState("1er dialog");
+    const [background, setBackground] = React.useState(require('../assets/pauvre_jour.png'));
 
     const handleDialog = (dialog) => {
         setDialog(dialog);
     }
 
+    const modifyBackground = (background) => {
+        setBackground(background);
+    }
+
     return (
         <div className="Game">
             <div className="Game-context">
-                <div className="Game-background">
-                    <img src="https://avatars.githubusercontent.com/u/55129304?v=4" alt="background" />
-                </div>
+                <img className="Game-background" src={background} alt="background" />
                 <div className="Game-person-left">
-                    <img src="https://avatars.githubusercontent.com/u/55129304?v=4" alt="person" />
+                    <img src="https://avatars.githubusercontent.com/u/55129304?v=4" alt="person-left" />
                 </div>
                 <div className="Game-person-right">
-                    <img src="https://avatars.githubusercontent.com/u/55129304?v=4" alt="person" />
+                    <img src="https://avatars.githubusercontent.com/u/55129304?v=4" alt="person-right" />
                 </div>
             </div>
             <div className="Game-dialog">
