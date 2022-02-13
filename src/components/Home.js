ffe7fcb015dac485f59f402ec0302d1646f8ffa6
Austin-William Lo <austin-william.lo@epitech.eu>
2022-02-13 21:30:45 +0100

    [FIX] orthographes

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ function HomePage() {
       </h1>
       <div className="Home-description">
         <p>
-          Henry, un jeune homme de 29 ans, vit dans un monde où le temps à remplacer l'argent. Ce jeune homme travaille dans une usine pour survivre et gagner une certaine somme le temps de vivre une journée.
+          Henry, un jeune homme de 29 ans, vit dans un monde où le temps à remplacé l'argent. Ce jeune homme travaille dans une usine pour survivre et gagner une certaine somme le temps de vivre une journée.
         </p>
         <p>
           Il y a les riches et les pauvres. Les riches vivent plus longtemps et les pauvres peuvent mourir à tout moment. Lors d'un soir banal, la mère d'Henry remarqua que les prix des billets de bus ont doublé. Par manque de temps, elle ne peut payer son ticket et est forcée de parcourir tout le trajet à pied. Comme vous l'avez pu voir dans le trailer, Henry sera obligé de faire des choix dont <strong>VOUS</strong> choisirez.
